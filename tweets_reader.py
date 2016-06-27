00e3631beea32994d55df1c4c0dc890eb46a6c37
athityakumar <athityakumar@gmail.com>
2016-06-27 12:22:38 +0530

    Added model generator

--- tweets_reader.py
+++ tweets_reader.py
@@ -7,8 +7,11 @@ from stop_words import get_stop_words
 from nltk.stem.porter import PorterStemmer
 from gensim import corpora, models
 import gensim
+import numpy as np
 import MySQLdb
 import string
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.decomposition import NMF
 
 def database_login() :
   return MySQLdb.connect(host="localhost",user="root",passwd="password",db="database") 
@@ -48,6 +51,17 @@ def get_all_tweets() :
   database_end()
   return tweets
 
+def clean(model,index) :
+  index = index + 1
+  dirty = ["+",".","*"]
+  while model.find("0.") != -1 :
+    prob = model[model.find("0."):model.find("0.")+6]
+    model = string.replace(model,prob,"")
+  for element in dirty :    
+    model = string.replace(model,element,"")
+  model = "Topic #"+str(index)+": "+model
+  return model
+
 all_tweets = get_all_tweets()  
 doc_set = []
 count = 0
@@ -57,6 +71,7 @@ for index in all_tweets :
 
 tokenizer = RegexpTokenizer(r'\w+')
 en_stop = get_stop_words('en')
+en_stop.extend(["s","t","rs","http","amp","rt","https","0","1","2","3","4","5","6","7","8","9"])
 p_stemmer = PorterStemmer()  
 texts = []
 
@@ -68,7 +83,7 @@ for i in doc_set:
     tokens = tokenizer.tokenize(raw)
 
     # remove stop words from tokens
-    stopped_tokens = [i for i in tokens if not i in en_stop]
+    stopped_tokens = [i for i in tokens if not i.lower() in en_stop]
     
     # stem tokens
     stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
@@ -82,6 +97,12 @@ dictionary = corpora.Dictionary(texts)
 # convert tokenized documents into a document-term matrix
 corpus = [dictionary.doc2bow(text) for text in texts]
 
-# generate LDA model
-ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=10, id2word = dictionary)
-print(ldamodel.print_topics(num_topics=count, num_words=10))
+#generate LDA model
+num_topics = 20
+num_words = 10
+ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=num_topics, id2word = dictionary)
+
+i = 0
+while i < num_topics:
+  print(clean(ldamodel.print_topics(num_topics=num_topics,num_words=num_words)[i][1],i))
+  i = i+1
