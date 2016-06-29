dfb07ca7ee829b258fc36d742d75591be5e2be93
athityakumar <athityakumar@gmail.com>
2016-06-30 00:07:01 +0530

    Added option of scraping #technology tweets from tweets_creator.rb

--- scripts/athityakumar/tweets_creator.rb
+++ scripts/athityakumar/tweets_creator.rb
@@ -2,9 +2,19 @@ begin
   puts "Enter directory to our rails project : (/home/user/...) "
   dir = gets.chomp
   Dir.chdir(dir)
-  while true
-    system "rake twitter_status:fetch_tweets"
-  end
+  puts "1: Home time-line tweets \n2: Search by key-word tweets \nEnter your choice : "
+  c = gets.chomp.to_i
+  if c == 1
+    while true
+      system "rake twitter_status:fetch_tweets"
+    end
+  else
+    puts "Enter search query : (#technology)"
+    q = gets.chomp
+    while true
+      system "rake 'twitter_status:fetch_tweets_search[#{q}]'"
+    end
+  end  
 rescue
   puts "Some error."
 end    
